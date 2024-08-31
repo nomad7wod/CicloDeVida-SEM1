be5e954f8b0a594cdde5b4b0893e38f90531cec1
nomad7wod <hernanwly@gmail.com>
2024-08-31 11:58:51 -0500

    Ciclo de vida del MainActivity

--- app/src/main/java/com/example/ciclodevida_sem1/MainActivity.kt
+++ app/src/main/java/com/example/ciclodevida_sem1/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.ciclodevida_sem1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val tag: String = "INF-STATE"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +19,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         //Hernan Marecos primer comentario KOTLIN
+        Log.i(tag,"MainActivity-->onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag,"MainActivity-->onStart")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag,"MainActivity-->onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag,"MainActivity-->onStop")
+    }
+
+
 }
+
+
